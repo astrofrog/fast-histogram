98ef20df1075d8b0fb3328541acf2be77dcd11ac
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-17 21:41:34 +0100

    Adapt setup.py to work without Cython for tarfile releases

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 import os
 
 import numpy as np
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from setuptools import setup
+from setuptools.extension import Extension
 
-extensions = [Extension("fast_histogram.histogram_cython",
-                        [os.path.join('fast_histogram', 'histogram_cython.pyx')],
-                        include_dirs=[np.get_include()])]
+if os.path.exists(os.path.join('fast_histogram', 'histogram_cython.pyx')):
+
+    extensions = [Extension("fast_histogram.histogram_cython",
+                            [os.path.join('fast_histogram', 'histogram_cython.pyx')],
+                            include_dirs=[np.get_include()])]
+
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+    install_requires = ['numpy', 'Cython']
+
+else:
+
+    extensions = [Extension("fast_histogram.histogram_cython",
+                            [os.path.join('fast_histogram', 'histogram_cython.c')],
+                            include_dirs=[np.get_include()])]
+
+    install_requires = ['numpy', 'Cython']
 
 setup(name='fast-histogram',
       version='0.1.dev0',
-      install_requires=['numpy', 'Cython'],
+      install_requires=install_requires,
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
       license='BSD',
       url='https://github.com/astrofrog/fastogram',
       packages=['fast_histogram'],
-      ext_modules=cythonize(extensions))
+      ext_modules=extensions)
