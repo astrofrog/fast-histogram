51cad524dd10ed7831149ab10e4bd7755d0f6a7a
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-12 12:10:44 +0000

    Make it so that numpy is not required to run setup.py and is auto-installed if needed.

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 import os
 import io
 
-import numpy as np
 from setuptools import setup
 from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
+
+
+class build_ext_with_numpy(build_ext):
+    def finalize_options(self):
+        super(build_ext_with_numpy, self).finalize_options()
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 extensions = [Extension("fast_histogram._histogram_core",
-                        [os.path.join('fast_histogram', '_histogram_core.c')],
-                        include_dirs=[np.get_include()])]
+                        [os.path.join('fast_histogram', '_histogram_core.c')])]
 
 with io.open('README.rst', encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
@@ -16,10 +23,12 @@ setup(name='fast-histogram',
       version='0.4.dev0',
       description='Fast simple 1D and 2D histograms',
       long_description=LONG_DESCRIPTION,
+      setup_requires=['numpy'],
       install_requires=['numpy'],
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
       license='BSD',
       url='https://github.com/astrofrog/fast-histogram',
       packages=['fast_histogram', 'fast_histogram.tests'],
-      ext_modules=extensions)
+      ext_modules=extensions,
+      cmdclass={'build_ext': build_ext_with_numpy})
