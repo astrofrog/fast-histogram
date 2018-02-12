ba437857b25c3831c7a75bb8bc1092e5be107d4b
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-12 11:58:57 +0000

    Fix bug with reading in README.rst file

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import os
+import io
 
 import numpy as np
 from setuptools import setup
 from setuptools.extension import Extension
 
 extensions = [Extension("fast_histogram._histogram_core",
-                       [os.path.join('fast_histogram', '_histogram_core.c')],
-                       include_dirs=[np.get_include()])]
+                        [os.path.join('fast_histogram', '_histogram_core.c')],
+                        include_dirs=[np.get_include()])]
+
+with io.open('README.rst', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(name='fast-histogram',
       version='0.4.dev0',
       description='Fast simple 1D and 2D histograms',
-      long_description=open('README.rst').read(),
+      long_description=LONG_DESCRIPTION,
       install_requires=['numpy'],
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
