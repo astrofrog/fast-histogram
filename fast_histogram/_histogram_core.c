0fff46c1768fd9c55f97e16c396cd33a60e55ad0
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-10-17 13:40:05 +0100

    Bumped minimum required Python version to 3.8

--- fast_histogram/_histogram_core.c
+++ fast_histogram/_histogram_core.c
@@ -1,5 +1,5 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#define Py_LIMITED_API 0x030600f0
+#define Py_LIMITED_API 0x030800f0
 
 #include <Python.h>
 #include <numpy/arrayobject.h>
@@ -35,21 +35,13 @@ static PyMethodDef module_methods[] = {
 
 /* This is the function that is called on import. */
 
-#if PY_MAJOR_VERSION >= 3
-  #define MOD_ERROR_VAL NULL
-  #define MOD_SUCCESS_VAL(val) val
-  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
-  #define MOD_DEF(ob, name, doc, methods) \
-          static struct PyModuleDef moduledef = { \
-            PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
-          ob = PyModule_Create(&moduledef);
-#else
-  #define MOD_ERROR_VAL
-  #define MOD_SUCCESS_VAL(val)
-  #define MOD_INIT(name) void init##name(void)
-  #define MOD_DEF(ob, name, doc, methods) \
-          ob = Py_InitModule3(name, methods, doc);
-#endif
+#define MOD_ERROR_VAL NULL
+#define MOD_SUCCESS_VAL(val) val
+#define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+#define MOD_DEF(ob, name, doc, methods) \
+        static struct PyModuleDef moduledef = { \
+        PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+        ob = PyModule_Create(&moduledef);
 
 MOD_INIT(_histogram_core)
 {
