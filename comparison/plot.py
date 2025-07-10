994802e1c5dc65c16325fc7e37ff8b37d509d900
prajwel <prajwel.pj@gmail.com>
2025-07-10 17:37:22 +0530

    Benchmark on histogramdd added.

--- comparison/plot.py
+++ comparison/plot.py
@@ -17,17 +17,24 @@ import numpy as np
     fa_2d_min,
     fa_2d_mean,
     fa_2d_median,
+    np_3d_min,
+    np_3d_mean,
+    np_3d_median,
+    fa_3d_min,
+    fa_3d_mean,
+    fa_3d_median,
 ) = np.loadtxt("benchmark_times.txt", unpack=True)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(size, np_1d_min / fa_1d_min, color=(34 / 255, 122 / 255, 181 / 255), label="1D")
-ax.plot(size, np_2d_min / fa_2d_min, color=(255 / 255, 133 / 255, 25 / 255), label="2D")
+ax.plot(size, np_1d_min / fa_1d_min, label="1D")
+ax.plot(size, np_2d_min / fa_2d_min, label="2D")
+ax.plot(size, np_3d_min / fa_3d_min, label="DD (3D)")
 ax.set_xscale("log")
 ax.set_xlim(0.3, 3e8)
-ax.set_ylim(1, 35)
+ax.set_ylim(1, 20)
 ax.grid()
 ax.set_xlabel("Array size")
-ax.set_ylabel("Speedup (fast-histogram / numpy)")
+ax.set_ylabel(f"Speedup (fast-histogram / numpy (version {np.__version__})")
 ax.legend()
 fig.savefig("speedup_compared.png", bbox_inches="tight")
