2bac6fcaf50db8201297f7bb2f395d4ffe655277
Thomas Robitaille <thomas.robitaille@gmail.com>
2026-03-02 22:37:40 +0000

    More improvements to meson set-up

--- fast_histogram/_histogram_core.c
+++ fast_histogram/_histogram_core.c
@@ -1,5 +1,5 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#define Py_LIMITED_API 0x030900f0
+// Py_LIMITED_API is defined by meson via limited_api argument
 
 #include <Python.h>
 #include <numpy/arrayobject.h>
